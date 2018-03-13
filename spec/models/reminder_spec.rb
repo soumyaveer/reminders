075895975bfdfea7eb3446486376bff610f6dd4d
Soumya Veer <veer.soumya@gmail.com>
2018-03-13 13:41:23 -0700

    add rspec tests for Reminder#Model validations

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -1,5 +1,32 @@
-require 'rails_helper'
+describe Reminder do
+  describe 'validations' do
+    before do
+      @reminder = FactoryBot.create(:reminder)
+    end
 
-RSpec.describe Reminder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it 'fails if title is not present' do
+      @reminder.title = nil
+
+      expect(@reminder.valid?).to eql(false)
+      expect(@reminder.errors[:title]).to be_present
+    end
+
+    it 'fails if time is not present' do
+      @reminder.time = nil
+
+      expect(@reminder.valid?).to eql(false)
+      expect(@reminder.errors[:time]).to be_present
+    end
+
+    it 'fails if recipient\'s email addresses are not present' do
+      @reminder.recipient_email_addresses = nil
+
+      expect(@reminder.valid?).to eql(false)
+      expect(@reminder.errors[:recipient_email_addresses]).to be_present
+    end
+
+    it 'passes if required attributes are present' do
+      expect(@reminder.valid?).to eql(true)
+    end
+  end
 end
