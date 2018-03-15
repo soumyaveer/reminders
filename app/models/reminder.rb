d96d5afe9e2df29c139c56382eaf2d6c99933b61
Soumya Veer <veer.soumya@gmail.com>
2018-03-15 12:21:01 -0700

    add sidekiq_job_id to reminders table

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -5,4 +5,9 @@ class Reminder < ApplicationRecord
   def recipient_email_address_values=(values)
     self.recipient_email_addresses = values.split(",")
   end
+
+  after_create :enqueue_sidekiq_job
+  after_destroy :remove_sidekiq_job
+  after_update :re_enqueue_sidekiq_job
+
 end
