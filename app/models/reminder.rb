41292029b12f4fd871ecbcfef71922a02c83e4d5
Soumya Veer <veer.soumya@gmail.com>
2018-03-15 12:22:00 -0700

    add method to enqueue sidekiq job

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -10,4 +10,11 @@ class Reminder < ApplicationRecord
   after_destroy :remove_sidekiq_job
   after_update :re_enqueue_sidekiq_job
 
+  private
+
+  def enqueue_sidekiq_job
+    sidekiq_job_id = EmailWorker.perform_at(self.time, self.id)
+    self.update_column(:sidekiq_job_id, sidekiq_job_id)
+  end
+
 end
