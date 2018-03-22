ad516f7c7690c8d6b927e18f5d7c54edcec1076c
Soumya Veer <veer.soumya@gmail.com>
2018-03-22 12:39:28 -0700

    add ReactRouter and create root_path

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import RemindersListItem from "./components/RemindersListItem";
+import RemindersForm from "./components/RemindersForm";
 
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
