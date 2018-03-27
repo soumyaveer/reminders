8a492e55f8c7e8a46f75c828f05ef6cceed2bbbb
Soumya Veer <veer.soumya@gmail.com>
2018-03-26 22:59:21 -0700

    Turn RemindersListItem component into a stateless component

--- client/src/components/RemindersListItem.js
+++ client/src/components/RemindersListItem.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class RemindersListItem extends React.Component {
+const RemindersListItem = (props) => {
+  const deleteConfirmationPath = `/reminders/${props.reminder.id}/delete_confirmation`;
+  const handleClick = () => { return props.onClick(props.reminder.id) };
 
-  handleOnClick = () => {
-    this.props.onClick(this.props.reminder.id)
-  };
+  return (
+    <div className="tile">
+      <Link className="deleteLink" to={deleteConfirmationPath}>
+        x
+      </Link>
 
-  render() {
-    const deleteConfirmationUrl = `/reminders/${this.props.reminder.id}/delete_confirmation`;
-    return (
-      <div className="tile">
-        <Link className="deleteLink" to={deleteConfirmationUrl} >
-            x
-        </Link>
-
-        <Link to={`/reminders/${this.props.reminder.id}`} className="tile-link">
-          <h4 onClick={this.handleOnClick}>
-            {this.props.reminder.title}
-          </h4>
-        </Link>
-
-          <p onClick={this.handleOnClick}>
-            {this.props.reminder.message}
-          </p>
-      </div>
-        )
-      }
-  }
+      <Link to={`/reminders/${props.reminder.id}`} className="tile-link">
+        <h4 onClick={handleClick}>
+          {props.reminder.title}
+        </h4>
+      </Link>
+      <p onClick={handleClick}>
+        {props.reminder.message}
+      </p>
+    </div>
+  )
+};
 
 export default RemindersListItem;
