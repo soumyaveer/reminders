912a0096070eb5adbf596d6084a264e9dc09271c
Soumya Veer <veer.soumya@gmail.com>
2018-04-17 16:43:36 -0700

    add likes to schema in rails API

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315191859) do
+ActiveRecord::Schema.define(version: 20180417233949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180315191859) do
     t.datetime "updated_at", null: false
     t.string "recipient_email_addresses", default: [], array: true
     t.string "sidekiq_job_id"
+    t.integer "likes", default: 0
   end
 
 end
